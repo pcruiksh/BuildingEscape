1a3ffa43c54596a32cad5d5beb25a268cacb96ea
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2018-03-03 22:20:42 -0500

    determining actor position and setting door rotation

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,11 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Patrick Cruikshank 2018
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,17 +16,17 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto Object = GetOwner();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s\n"), *Object->GetName(), *Object->GetActorLocation().ToString());
+	AActor* Door = GetOwner();
+	Door->SetActorRotation( Door->GetActorRotation().Add(0.f, -90.f, 0.f) );
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
