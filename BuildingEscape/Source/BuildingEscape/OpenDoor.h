06870865888c06122986df636ce91e8c08ec0304
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2018-03-10 10:23:33 -0500

    add uproperties and associate trigger volume with doors

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+
 #include "OpenDoor.generated.h"
 
 
@@ -11,6 +13,13 @@ UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume * PressurePlate;
+
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.f;
 
 public:	
 	// Sets default values for this component's properties
